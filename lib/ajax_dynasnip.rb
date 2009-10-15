5f8d338964af64909797b42aadca44c9f2a3320b
James Adam <james@lazyatom.com>
2009-10-15 06:35:21 +0100

    Implemented a simple demonstration of ansynchronously-loaded dynasnips. This should be useful for writing dynasnips that make calls to external services, like fetching delicious feeds or something like that.

---
+++ lib/ajax_dynasnip.rb
@@ -0,0 +1,32 @@
+require 'vanilla/dynasnip'
+
+class AjaxDynasnip < Dynasnip
+  def handle(*args)
+    if app.request.instance_eval { @rack_request }.xhr?
+      __normal_handle
+    else
+      url = url_to(__snip_name)
+      %{<a class="vanilla_ajax" href="#{url}">#{__snip_name}</a>}
+    end
+  end
+
+  private
+  
+  def __snip_name
+    # borrowed from ActiveSupport
+    self.class.name.split("::").last.gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+    gsub(/([a-z\d])([A-Z])/,'\1_\2').
+    tr("-", "_").
+    downcase
+  end
+
+  # When subclasses define 'handle', we actually want to move that out of
+  # the way, so that the parent definition is called; it will decide whether
+  # or not to generate the Ajax javascript, or actually run the dynasnip
+  def self.method_added(method)
+    unless method.to_s =~ /^__normal_/
+      alias_method :"__normal_#{method}", method
+      remove_method method
+    end
+  end
+end
--- public/javascripts/vanilla.js
+++ public/javascripts/vanilla.js
@@ -1,22 +1,34 @@
-function vanillaPrepareEditor() {
-  $('input.attribute_name').change(function() {
-    var dl_children = $('dl.attributes').children();
-    var dt_index = dl_children.index(this.parentNode) + 1;
-    var textarea_for_this = dl_children[dt_index].childNodes[0];
-    textarea_for_this.name = this.value;
-  });
-  $('textarea').autogrow();
+Vanilla = {
+  linkEditorInputs: function() {
+    $('input.attribute_name').change(function() {
+      var dl_children = $('dl.attributes').children();
+      var dt_index = dl_children.index(this.parentNode) + 1;
+      var textarea_for_this = dl_children[dt_index].childNodes[0];
+      textarea_for_this.name = this.value;
+    });
+    $('textarea').autogrow();
+  },
+  
+  prepareEditor: function() {
+    $('a#add').click(function() {
+      $('dl.attributes').append('<dt><input class="attribute_name" type="text"></input></dt><dd><textarea></textarea></dd>');
+      Vanilla.linkEditorInputs();
+      return false;
+    });
+    Vanilla.linkEditorInputs();
+  },
+  
+  setupAjaxDynasnips: function() {
+    $('a.vanilla_ajax').hide().each(function() {
+      var element = $(this);
+      $.get(element[0].href + '.text', function(content) {
+        element.after(content).remove();
+      });
+    });
+  }
 }
 
-$(document).ready(function() {
-
-  $('a#add').click(function() {
-    $('dl.attributes').append('<dt><input class="attribute_name" type="text"></input></dt><dd><textarea></textarea></dd>');
-    vanillaPrepareEditor();
-    return false;
-  });
-  
-  vanillaPrepareEditor();
-
+$(function() {
+  Vanilla.prepareEditor();
+  Vanilla.setupAjaxDynasnips();
 });
-
