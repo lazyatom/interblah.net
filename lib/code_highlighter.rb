ce8e9d06e3499dc16d4376c48bafa0dc1ee5d218
James Adam <james@lazyatom.com>
2009-11-24 11:04:37 +0000

    Changed code snip to refer to enclosing snip.

---
+++ lib/code_highlighter.rb
@@ -0,0 +1,9 @@
+require 'syntax/convertors/html'
+class CodeHighlighter < Dynasnip
+  def handle(language, part_to_render='content')
+    text = enclosing_snip.__send__(part_to_render.to_sym)
+    convertor = Syntax::Convertors::HTML.for_syntax(language)
+    code = convertor.convert(text, false)
+    %(<pre class="code ) + language + %("><code>) + code + %(</code></pre>)
+  end
+end
