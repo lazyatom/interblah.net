8207756c945c563ba50bd657f4be41420a15ff1b
James Adam <james@lazyatom.com>
2010-11-21 22:35:33 +0000

    The  part of app in lazyatom-deploy doesn't seem to work; it outputs 'main'.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,6 +5,7 @@ set :environment, 'production'
 manifest :app, %{
   lazyatom::app {"interblah.net":
     deploy_to => "<%= deploy_to %>",
+    domain => "interblah.net",
     vhost_additions => [
       'Alias /assets "<%= deploy_to %>/shared/assets"',
       'RedirectMatch 301 ^/2006/\d+/\d+/(.+) "/$1"',
