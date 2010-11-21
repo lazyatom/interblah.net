a6a5bdcb744fe68ed8a2c86df616febc0dc46202
James Adam <james@lazyatom.com>
2010-11-21 21:28:27 +0000

    Multiline strings weren't working.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,15 +3,10 @@ set :default_stage, "production"
 
 require 'freerange/deploy'
 require 'freerange/puppet'
+require 'lazyatom/deploy'
 
 set :repository, 'git@github.com:lazyatom/interblah.net.git'
 set :application, 'interblah.net'
-
-set :user, `whoami`.strip
-set :group, 'application'
-
-set :puppet_user, 'root'
-set :puppet_os, 'ubuntu'
 
 namespace :deploy do
   task :migrate do ; end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,11 +5,11 @@ set :environment, 'production'
 manifest :app, %{
   lazyatom::app {"interblah.net":
     deploy_to => "<%= deploy_to %>",
-    vhost_additions => "
-Alias /assets \"<%= deploy_to %>/shared/assets\"
-RedirectMatch 301 ^/2006/\d+/\d+/(.+) \"/$1\"
-RedirectMatch 301 ^/2007/\d+/\d+/(.+) \"/$1\"
-RedirectMatch 301 ^/2008/\d+/\d+/(.+) \"/$1\"
-    "
+    vhost_additions => [
+      'Alias /assets "<%= deploy_to %>/shared/assets"',
+      'RedirectMatch 301 ^/2006/\d+/\d+/(.+) "/$1"',
+      'RedirectMatch 301 ^/2007/\d+/\d+/(.+) "/$1"',
+      'RedirectMatch 301 ^/2008/\d+/\d+/(.+) "/$1"'
+    ]
   }
 }
