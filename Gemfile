9c3752dd3c17f5544dfe357b06ba7d91c3ed9224
James Adam <james@lazyatom.com>
2011-09-16 23:36:52 +0100

    Try out a simpler deployment recipe.

--- Gemfile
+++ Gemfile
@@ -8,6 +8,4 @@ gem "compass"
 gem "kramdown"
 gem "rake"
 
-gem 'freerange-deploy', :git => "git://github.com/freerange/deploy.git"
-gem 'freerange-puppet', :git => "git://github.com/freerange/freerange-puppet.git"
-gem 'lazyatom-deploy', :git => "git://github.com/lazyatom/lazyatom-deploy.git"
+gem 'tomafro-deploy', :git => "git://github.com/tomafro/tomafro-deploy.git"
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,13 @@
-set :stages, %w(production)
-set :default_stage, "production"
+require 'tomafro/deploy'
 
-require 'freerange/deploy'
-require 'freerange/puppet'
-require 'lazyatom/deploy'
+server "interblah.net", :app
 
+set :application_user, "interblah"
 set :repository, 'git@github.com:lazyatom/interblah.net.git'
 set :application, 'interblah.net'
 
 namespace :deploy do
-  task :migrate do ; end
+  task :restart do
+    run "cd #{deploy_to} && mkdir -p tmp && touch tmp/restart.txt"
+  end
 end
