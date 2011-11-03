39929a04d287d7bb4cf883b9e8a4d84dde9c86ed
James Adam <james@lazyatom.com>
2011-11-03 23:39:20 +0000

    Fix up the deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,11 @@
-require 'tomafro/deploy'
+require 'recap/deploy'
 
 server "interblah.net", :app
 
 set :application_user, "interblah"
 set :repository, 'git@github.com:lazyatom/interblah.net.git'
 set :application, 'interblah.net'
+set :deploy_to, "/var/apps/#{application}"
 
 namespace :deploy do
   task :restart do
