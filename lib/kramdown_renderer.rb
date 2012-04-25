5a3f0999b5e930c17529ae07f64fabc2a64c5028
James Adam <james@lazyatom.com>
2012-04-25 23:44:39 +0100

    Remove debug output.

--- lib/kramdown_renderer.rb
+++ lib/kramdown_renderer.rb
@@ -2,7 +2,6 @@ require 'kramdown/document'
 
 class Vanilla::Renderers::Kramdown < ::Vanilla::Renderers::Base
   def process_text(content)
-    puts "processing"
     Kramdown::Document.new(content).to_html
   end
 end
