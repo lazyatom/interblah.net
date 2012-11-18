556b05e6f965b1c88a76d1230b3a8503c89586df
James Adam <james@lazyatom.com>
2012-11-18 22:54:28 +0000

    Escape curly braces in code output.

--- lib/code_highlighter.rb
+++ lib/code_highlighter.rb
@@ -4,6 +4,6 @@ class CodeHighlighter < Dynasnip
     text = enclosing_snip.__send__(part_to_render.to_sym)
     convertor = Syntax::Convertors::HTML.for_syntax(language)
     code = convertor.convert(text, false)
-    %(<pre class="code ) + language + %("><code>) + code + %(</code></pre>)
+    %(<pre class="code ) + language + %("><code>) + self.class.escape_curly_braces(code) + %(</code></pre>)
   end
 end
