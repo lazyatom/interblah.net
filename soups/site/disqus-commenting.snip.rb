6400a67996b0aee7c85067855f87061a9168c2c0
James Adam <james@lazyatom.com>
2013-01-03 13:27:26 -0600

    Add commenting to blog posts.

---
+++ soups/site/disqus-commenting.snip.rb
@@ -0,0 +1,27 @@
+class DisqusCommenting < Dynasnip
+  CONTENT = <<-EOS
+<hr class="separator" />
+<div id="disqus_thread"></div>
+<script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = 'interblah'; // required: replace example with your forum shortname
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+EOS
+
+  def handle(*args)
+    if app.request.snip.kind == "blog"
+      CONTENT
+    end
+  end
+
+  self
+end
