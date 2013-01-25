b6f03521a41b0ebb0c3d1eef6b0220690439f887
James Adam <james@lazyatom.com>
2013-01-25 15:41:28 +0000

    Use pygments for syntax highlighting.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source :rubygems
 
 gem "vanilla", git: "https://github.com/lazyatom/vanilla-rb.git"
-gem "syntax"
+gem "pygments.rb"
 gem "redcarpet"
 gem "haml", "~>3.1"
 gem "susy"
--- lib/code_highlighter.rb
+++ lib/code_highlighter.rb
@@ -1,9 +1,8 @@
-require 'syntax/convertors/html'
+require "pygments"
 class CodeHighlighter < Dynasnip
   def handle(language, part_to_render='content')
     text = enclosing_snip.__send__(part_to_render.to_sym)
-    convertor = Syntax::Convertors::HTML.for_syntax(language)
-    code = convertor.convert(text, false)
-    %(<pre class="code ) + language + %("><code>) + self.class.escape_curly_braces(code) + %(</code></pre>)
+    code = Pygments.highlight(text, :lexer => language, :options => {:encoding => 'utf-8'})
+    %(<div class="code ) + language + %(">) + self.class.escape_curly_braces(code) + %(</div>)
   end
 end
