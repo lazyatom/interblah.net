581721f19757afabb6ade3da4cd4cee87ce5423e
James Adam <james@lazyatom.com>
2020-07-21 10:09:32 +0100

    Remove old susy config file
    This isn't used at all now.

--- config.rb
+++
@@ -1,8 +0,0 @@
-require "susy"
-
-http_path = "/"
-css_dir = "public/stylesheets"
-sass_dir = "public/stylesheets/sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
-relative_assets = false
