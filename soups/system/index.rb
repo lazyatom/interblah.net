608af1b6921d9e7ccfb4deab51b25545f477df9d
James Adam <james@lazyatom.com>
2020-07-28 23:03:37 +0100

    Use l link snip on index
    This has the better title behaviour we want.

--- soups/system/index.rb
+++ soups/system/index.rb
@@ -1,7 +1,7 @@
 class Index < Vanilla::Dynasnip
   def get(*args)
     list = app.soup.all_snips.sort_by { |a| a.updated_at || Time.at(0) }.reverse.map { |snip|
-      %{<li>{link_to "#{snip.name}"}</li>}
+      %{<li>{l "#{snip.name}"}</li>}
     }.join
     %{<ol id="index">#{list}</ol>}
   end
