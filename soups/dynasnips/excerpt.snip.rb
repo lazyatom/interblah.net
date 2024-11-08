e5447efa959222d8b624e8d8dc3ad67226b06ae0
James Adam <james@lazyatom.com>
2024-11-08 13:41:46 +0000

    Ignore blank lines in excerpt line count

--- soups/dynasnips/excerpt.snip.rb
+++ soups/dynasnips/excerpt.snip.rb
@@ -1,5 +1,5 @@
 dynasnip do |snip_name, lines = 3|
   snip = app.soup[snip_name]
-  lines = app.render(snip).split("\n")[0..lines.to_i]
+  lines = app.render(snip).split("\n").reject { |l| l.strip == '' }[0..lines.to_i]
   lines.join("\n")
 end
